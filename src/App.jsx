994d7c76142ce68b00b38f420881627440e9443d
Guilherme Gomes Neto <88989905+guilhermegomesneto@users.noreply.github.com>
2023-07-03 11:27:26 -0300

    Router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Link, Outlet } from "react-router-dom"
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
+      <nav id="navbar">
+        <h2>
+          <Link to="/">Something to Watch</Link>
+        </h2>
+        <Link to="/movie/1">Movie</Link>
+        <Link to="/search">Search</Link>
+      </nav>
       <h2>Something to Watch</h2>
+      <Outlet />
     </div>
-  )
+  );
 }
 
 export default App
