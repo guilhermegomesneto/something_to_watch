d30658e8dad3f899108372dfca4c9711d743c685
Guilherme Gomes Neto <88989905+guilhermegomesneto@users.noreply.github.com>
2023-07-05 12:28:06 -0300

    Search function

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+import "./MovieGrid.css";
+
+const searchURL = import.meta.env.VITE_SEARCH_MOVIE;
+const tmdbKey = import.meta.env.VITE_API_KEY;
+
 const Search = () => {
-    return <div>Search</div>
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchResultsURL = `${searchURL}?${tmdbKey}&query=${query}`;
+
+    getSearchedMovies(searchResultsURL);
+  }, [query]);
+
+  return (
+      <div className="container">
+      <h2 className="title">Search Results</h2>
+      <div className="movies-container">
+          {movies.length === 0 && <p>Loading...</p>}
+          {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+      </div>
+  );
 }
   
 export default Search;
