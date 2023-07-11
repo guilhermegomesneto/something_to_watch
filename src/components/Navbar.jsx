deabb4fc3dffd657d3495e978edf922556ca1bfa
Guilherme Gomes Neto <88989905+guilhermegomesneto@users.noreply.github.com>
2023-07-11 00:33:08 -0300

    Improving Responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,15 +22,17 @@ const Navbar = () => {
         <h2>
           <Link to="/"><BiMoviePlay /> Something to Watch</Link>
         </h2>
-        <h3>
-          <Link to="trending">Trending</Link>
-        </h3>
-        <h3>
-          <Link to="top-rated">Top Rated</Link>
-        </h3>
-        <h3>
-          <Link to="upcoming">Upcoming</Link>
-        </h3>
+        <div className="pages">
+          <h3>
+            <Link to="trending">Trending</Link>
+          </h3>
+          <h3>
+            <Link to="top-rated">Top Rated</Link>
+          </h3>
+          <h3>
+            <Link to="upcoming">Upcoming</Link>
+          </h3>
+        </div>
         <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Search for a movie" onChange={(e) => setSearch(e.target.value)} value={search} />
           <button type="submit">
